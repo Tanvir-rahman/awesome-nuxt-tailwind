feef876971152e63c673d12c86f2d3a3879c6bd9
Tanvir Rahman <rahmantanvir@pathao.com>
2022-08-13 10:11:43 +0600

    Update: Added tailwind support and gitignore updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,23 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  tailwindcss: {
+    viewer: false,
+    config: {
+      darkMode: "class",
+      content: [
+        "./assets/**/*.css",
+        "./components/*.{vue,js}",
+        "./components/**/*.{vue,js}",
+        "./pages/*.vue",
+        "./pages/**/*.vue",
+        "./plugins/**/*.{js,ts}",
+        // "./*.{vue,js,ts}",
+        // "./nuxt.config.{js,ts}",
+      ],
+    },
+  },
+  buildModules: [
+    "@nuxtjs/tailwindcss",
+  ],
 })

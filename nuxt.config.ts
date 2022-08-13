9a99699d14070be0157ff93763c0d871503427a0
Tanvir Rahman <rahmantanvir@pathao.com>
2022-08-13 13:35:15 +0600

    feat: lint, prettier, husky setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,23 +2,54 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'nuxt-nuclues',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
   tailwindcss: {
     viewer: false,
     config: {
-      darkMode: "class",
+      darkMode: 'class',
       content: [
-        "./assets/**/*.css",
-        "./components/*.{vue,js}",
-        "./components/**/*.{vue,js}",
-        "./pages/*.vue",
-        "./pages/**/*.vue",
-        "./plugins/**/*.{js,ts}",
+        './assets/**/*.css',
+        './components/*.{vue,js}',
+        './components/**/*.{vue,js}',
+        './pages/*.vue',
+        './pages/**/*.vue',
+        './plugins/**/*.{js,ts}',
         // "./*.{vue,js,ts}",
         // "./nuxt.config.{js,ts}",
       ],
     },
   },
   buildModules: [
-    "@nuxtjs/tailwindcss",
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/stylelint
+    '@nuxtjs/stylelint-module',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
   ],
 })
